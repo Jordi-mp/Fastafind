c0050a2c36a4e6bed61f72e7d5ffc2607fb69cae
Jordi-mp <149062296+Jordi-mp@users.noreply.github.com>
2023-11-21 19:45:13 +0100

    Update Fastafind.sh

--- Fastafind.sh
+++ Fastafind.sh
@@ -19,3 +19,39 @@ echo " "
 echo Report of the files:
 echo "........................................................................."
 echo " "
+
+# Process each file 
+for i in $F; do 
+
+# Print a nice header including filename 
+echo "Processing file: $i" 
+
+# Determine if the file is nucleotide or amino acids based on content 
+if grep -q "NP" "$i"; then echo "Type: Protein fasta" 
+else echo "Type: Nucleotide fasta"
+ fi 
+
+# Number of sequences 
+echo "Number of sequences: $(grep -c ">" "$i")" 
+
+# Total sequence length (excluding gaps, spaces, newline characters) 
+echo "Sequence length: $(sed -n '/>/! { s/-//g; s/[:space:]//g; s/\///g; p; }' "$i" | tr -d '\n' | wc -m)"
+
+# Check if the file is a symlink
+ if [[ -h "$i" ]]; 
+then echo "Symlink: Yes"
+else echo "Symlink: No" 
+fi 
+echo " "
+
+# Display file content
+echo "File content:"
+ if [[ "$(wc -l < "$i")" -le $((2 * $Lines)) ]];
+ then cat "$i" 
+elif [[ "$(wc -l < "$i")" -eq 0 ]];
+ then echo "It has 0 lines" 
+else head -n "$Lines" "$i"
+ echo "..." 
+tail -n "$Lines" "$i" 
+fi 
+done
